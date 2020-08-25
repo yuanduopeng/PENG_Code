dc7b3792443dceff828525092083fdb33ecf1c83
PENG Yuanduo <yuanduo@PENGdeiMac.local>
2020-08-26 07:32:30 +0800

    add triangle loacation to splite image

--- 01_UDA/01_SelfDriving/Ex01/readImage.py
+++ 01_UDA/01_SelfDriving/Ex01/readImage.py
@@ -21,7 +21,9 @@ green_threshold = 200
 blue_threshold = 200
 rgb_threshold = [red_threshold, green_threshold, blue_threshold]
 
-thresholds = (img[:,:,0] < rgb_threshold[0]) | (img[:,:,1] < rgb_threshold[1]) | (img[:,:,2] < rgb_threshold[2])
+thresholds = (img[:,:,0] < rgb_threshold[0])\
+            | (img[:,:,1] < rgb_threshold[1])\
+            | (img[:,:,2] < rgb_threshold[2])
 # threshold:     -   [   true, true, true, true, true, true   ]   -
 # threshold:    |    [   true, true, true, true, true, true   ]    |
 # threshold:    |    [   true, true, true, true, true, true   ]    |
@@ -32,3 +34,11 @@ color_select[thresholds] = [0 ,0, 0]
 
 plt.imshow(color_select)
 plt.show()
+
+left_bottom = [0, 539]
+right_bottom = [900, 300]
+apex = [400, 0]
+
+fit_left = np.polyfit((left_bottom[0], apex[0]), (left_bottom[1], apex[1]), 1)
+fit_right = np.polyfit((right_bottom[0], apex[0]), (right_bottom[1], apex[1]), 1)
+fit_bottom = np.polyfit((left_bottom[0], right_bottom[0]), (left_bottom[1], right_bottom[1]), 1)
