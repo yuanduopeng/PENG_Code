95b6befbdaf78cb60159107b34c7e0a22f535281
PENG Yuanduo (CC-AD/EYM-CN) <Yuanduo.PENG@cn.bosch.com>
2020-08-26 18:24:17 +0800

    update to combine color and region

--- 01_UDA/01_SelfDriving/01_FindLaneLines/readImage.py
+++ 01_UDA/01_SelfDriving/01_FindLaneLines/readImage.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 img = mpimg.imread('test.jpg')
-xsize = img.shape[0]
-ysize = img.shape[1]
+ysize = img.shape[0]
+xsize = img.shape[1]
+
 # print(type(img), xsize, ysize)
 
 # img:     -   [   [r,g,b], [r,g,b], [r,g,b], [r,g,b], [r,g,b], [r,g,b]   ]   -
@@ -15,13 +16,15 @@ ysize = img.shape[1]
 # img:     -   [   [r,g,b], [r,g,b], [r,g,b], [r,g,b], [r,g,b], [r,g,b]   ]   -
 
 color_select = np.copy(img)
+region_select = np.copy(img)
+line_image = np.copy(img)
 
 red_threshold = 200
 green_threshold = 200
 blue_threshold = 200
 rgb_threshold = [red_threshold, green_threshold, blue_threshold]
 
-thresholds = (img[:,:,0] < rgb_threshold[0])\
+color_thresholds = (img[:,:,0] < rgb_threshold[0])\
             | (img[:,:,1] < rgb_threshold[1])\
             | (img[:,:,2] < rgb_threshold[2])
 # threshold:     -   [   true, true, true, true, true, true   ]   -
@@ -30,15 +33,47 @@ thresholds = (img[:,:,0] < rgb_threshold[0])\
 # threshold:    |    [   true, true, true, true, true, true   ]    |
 # threshold:    |    [   true, true, true, true, true, true   ]    |
 # threshold:     -   [   true, true, true, true, true, true   ]   -
-color_select[thresholds] = [0 ,0, 0]
 
-plt.imshow(color_select)
-plt.show()
+# Mask dark pixels as BLACK
+color_select[color_thresholds] = [0 ,0, 0]
 
 left_bottom = [0, 539]
 right_bottom = [900, 300]
 apex = [400, 0]
 
+# Fit lines (y=Ax+B) to identify the  3 sided region of interest
+# np.polyfit() returns the coefficients [A, B] of the fit
+# np.ployfit( (point1[x-axel, point2[x-axel]), (point1[y-axel], point2[y-axel]) , degree of poly)
 fit_left = np.polyfit((left_bottom[0], apex[0]), (left_bottom[1], apex[1]), 1)
 fit_right = np.polyfit((right_bottom[0], apex[0]), (right_bottom[1], apex[1]), 1)
 fit_bottom = np.polyfit((left_bottom[0], right_bottom[0]), (left_bottom[1], right_bottom[1]), 1)
+
+# Find the region inside the lines
+XX, YY = np.meshgrid(np.arange(0, xsize), np.arange(0, ysize))
+print(XX.shape)
+print(img.shape)
+region_thresholds = (YY > (XX*fit_left[0] + fit_left[1])) & \
+                    (YY > (XX*fit_right[0] + fit_right[1])) & \
+                    (YY < (XX*fit_bottom[0] + fit_bottom[1]))
+
+# Color pixels red which are inside the region of interest
+region_select[region_thresholds] = [255, 0, 0]
+
+# Find where image is both colored right and in the region
+# [not black region (means white area) & selected region] = red color
+line_image[~color_thresholds & region_thresholds] = [255,0,0]
+
+# Display the image
+plt.subplot(221)
+plt.title('img')
+plt.imshow(img)
+plt.subplot(222)
+plt.title('color_select')
+plt.imshow(color_select)
+plt.subplot(223)
+plt.title('region_select')
+plt.imshow(region_select)
+plt.subplot(224)
+plt.title('line_image')
+plt.imshow(line_image)
+plt.show()
---
+++ 01_UDA/01_SelfDriving/01_FindLaneLines/test.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+a = 0
+b = ~a
+b = b & a
+print(b)
