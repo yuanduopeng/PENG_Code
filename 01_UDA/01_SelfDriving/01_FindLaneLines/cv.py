54f0c971f539cec59c80e2280a2bae0f3c6327c2
PENG Yuanduo (CC-AD/EYM-CN) <Yuanduo.PENG@cn.bosch.com>
2020-08-27 17:02:07 +0800

    draw lines in image

--- 01_UDA/01_SelfDriving/01_FindLaneLines/cv.py
+++ 01_UDA/01_SelfDriving/01_FindLaneLines/cv.py
@@ -13,12 +13,34 @@ kernel_size = 3
 img_blur = cv2.GaussianBlur(img_gray, (kernel_size, kernel_size), 0)
 
 # canny(input image, low, high threshold)
+# to show the edges in the image
 low_threshold = 50
 high_threshold = 150
 img_edges = cv2.Canny(img_blur, low_threshold, high_threshold)
+
+# Hough space(input image,
+#               rho: distance resolution,
+#               theta: angular resolution,
+#               threshold: number of votes ,
+#               np.array([]): empty np.array([]) is just a placeholder,
+#               minimum length of a line (in pixels),
+#               maximum distance between segments that you will allow to be
+#                   connected into a single line)
+rho = 1
+theta = np.pi/180
+threshold = 1
+min_line_length = 50
+max_line_gap = 10
+lines = cv2.HoughLinesP(img_edges, rho, theta, threshold, np.array([]),
+                                             min_line_length, max_line_gap)
+img_line = np.copy(img)*0
+for line in lines:
+    for x1, y1, x2, y2 in line:
+        cv2.line(img_line, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
 # show image
 cv2.imshow('img_gray', img_gray)
 cv2.imshow('img_blur', img_blur)
 cv2.imshow('img_edges', img_edges)
-cv2.waitKey(0) 
+cv2.imshow('img_line', img_line)
+cv2.waitKey(0)
