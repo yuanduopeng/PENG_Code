f5584e558b6a9e4873b1cc9d9704cd412b4df407
PENG Yuanduo (CC-AD/EYM-CN) <Yuanduo.PENG@cn.bosch.com>
2020-08-27 15:56:17 +0800

    update edge and blur param

--- 01_UDA/01_SelfDriving/01_FindLaneLines/cv.py
+++ 01_UDA/01_SelfDriving/01_FindLaneLines/cv.py
@@ -3,23 +3,21 @@ import matplotlib.image as mpimg
 import numpy as np
 import cv2
 
-image = mpimg.imread('exit-ramp.jpg')
-# plt.imshow(image)
-# plt.show()
-
 img = cv2.imread('exit-ramp.jpg')
 
 # cvtColor(input image, convert type)
 img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
 # GaussianBlur (input image, (ksize, ksize: positive and odd), )
-kernel_size = 5
+kernel_size = 3
 img_blur = cv2.GaussianBlur(img_gray, (kernel_size, kernel_size), 0)
 
-low_threshold = 100
-high_threshold = 150
 # canny(input image, low, high threshold)
+low_threshold = 50
+high_threshold = 150
 img_edges = cv2.Canny(img_blur, low_threshold, high_threshold)
+
+# show image
 cv2.imshow('img_gray', img_gray)
 cv2.imshow('img_blur', img_blur)
 cv2.imshow('img_edges', img_edges)
